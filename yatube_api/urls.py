ca4680bd19ebad28639a51e99ce47ab1f4708ed6
Allex-Nik <alexandr.nikolaev96@gmail.com>
2023-06-07 23:49:01 +0400

    1

--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -16,8 +16,8 @@ router.register('posts/(?P<post_id>[^/.]+)/comments',
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls)),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('api/v1/', include(router.urls)),
 ]
 
 
